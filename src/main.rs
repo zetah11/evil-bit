e497bae5e5ee97655bf76d645bd026737c4742d0
zetah11 <findusxi@protonmail.com>
2023-01-31 12:36:36 +0100

    :sparkles: save to image

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ mod expr;
 mod simplify;
 
 use std::io::Write;
+use std::path::PathBuf;
 
+use argh::FromArgs;
+use image::RgbImage;
 use rand::{thread_rng, Rng};
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
@@ -16,7 +19,7 @@ use self::simplify::simplify;
 
 /// An abstract value representing the maximum complexity of the generated
 /// expressions. A higher value means longer and more deeply nested expressions.
-const COMPLEXITY: usize = 50;
+const COMPLEXITY: usize = 160;
 
 /// The width (in pixels) of the resulting image.
 const WIDTH: usize = 30;
@@ -29,17 +32,28 @@ const HEIGHT: usize = 30;
 /// as it should, then the two images should be identical.
 const BEFORE: bool = false;
 
-fn main() {
-    const BITS: u8 = {
-        let w_bits = width_needed_for(WIDTH);
-        let h_bits = width_needed_for(HEIGHT);
+/// Generate images with random math!
+#[derive(FromArgs)]
+struct Args {
+    /// saves the generated image to the given path instead of printing it to
+    /// stdout.
+    #[argh(option, short = 'i')]
+    image: Option<PathBuf>,
+}
 
-        if w_bits > h_bits {
-            w_bits
-        } else {
-            h_bits
-        }
-    };
+const BITS: u8 = {
+    let w_bits = width_needed_for(WIDTH);
+    let h_bits = width_needed_for(HEIGHT);
+
+    if w_bits > h_bits {
+        w_bits
+    } else {
+        h_bits
+    }
+};
+
+fn main() {
+    let args: Args = argh::from_env();
 
     println!("{BITS} bit image");
 
@@ -52,6 +66,27 @@ fn main() {
     let expr = simplify::<BITS>(expr);
     println!("       into {expr}");
 
+    if let Some(path) = &args.image {
+        let image = write_to_image(expr);
+        image.save(path).unwrap();
+    } else {
+        write_to_stdout(before, expr)
+    }
+}
+
+#[must_use]
+fn write_to_image(expr: Expr) -> RgbImage {
+    let mut image = RgbImage::new(WIDTH as u32, HEIGHT as u32);
+    let drawing = draw::<BITS, _>(WIDTH, HEIGHT, expr);
+
+    for (pix, (r, g, b)) in image.pixels_mut().zip(drawing.into_iter().flatten()) {
+        pix.0 = [r, g, b];
+    }
+
+    image
+}
+
+fn write_to_stdout(before: Expr, after: Expr) {
     let mut stream = StandardStream::stdout(ColorChoice::Auto);
 
     if BEFORE {
@@ -72,7 +107,7 @@ fn main() {
         stream.write_all(b"becomes\n").unwrap();
     }
 
-    let drawing = draw::<BITS, _>(WIDTH, HEIGHT, expr);
+    let drawing = draw::<BITS, _>(WIDTH, HEIGHT, after);
 
     for row in drawing {
         for (r, g, b) in row {
